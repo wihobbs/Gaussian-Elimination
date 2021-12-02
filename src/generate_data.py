85d6c88bb49a4baab74c8b4e0c65eb7cc3830bf4
wihobbs <hobbsman58@gmail.com>
2021-12-02 11:27:32 -0500

    started project

---
+++ src/generate_data.py
@@ -0,0 +1,16 @@
+## Copyright 2021 William Hobbs
+import sys
+#from sage import ____ (rref)
+
+
+## Generate Matrices
+## Matrices need a "n" and "m" value, and then a
+## list of lists that generate values.
+
+class Matrix():
+    def __init__(self, n: int, v: List[int]):
+        this.n = n
+    
+## Use Sage to Row-Reduce the Matrices
+
+## Call C binaries (my code) to check their validity
