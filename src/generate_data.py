f6ee20a8feac628c202e7c523cae091b8521f1b4
wihobbs <hobbsman58@gmail.com>
2021-12-08 09:38:25 -0500

    added random matrix generator

--- src/generate_data.py
+++ src/generate_data.py
@@ -1,16 +1,35 @@
 ## Copyright 2021 William Hobbs
 import sys
+import List
+from random import randint
 #from sage import ____ (rref)
 
 
 ## Generate Matrices
-## Matrices need a "n" and "m" value, and then a
+## Matrices need an "n" value, and then a
 ## list of lists that generate values.
 
 class Matrix():
-    def __init__(self, n: int, v: List[int]):
-        this.n = n
+    def __init__(self, n: int, m: int):
+        assert(n % 1 == n and m % 1 == m)
+        self.n = n
+        self.m = m
+        self.v = self.generate(self.n, self.m)
     
+    def generate(n: int, m: int):
+        v = []
+        for i in range(n*m):
+            v[i] = randint(0, 100)
+        return v
+    
+    def toString(self):
+        print("n: " + self.n + "\nm: " + self.m + "\n")
+        
+
+Matrix(5, 4)
+
 ## Use Sage to Row-Reduce the Matrices
+def reduce(n: int, m: int, matrix: List[int]) -> List[int]:
+    return null
 
 ## Call C binaries (my code) to check their validity
--- src/rref.c
+++ src/rref.c
@@ -3,48 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 int main(int argc, char* argv[]) {
-    // Part One
-    FILE* input = fopen("data.txt", "r");
-    int count, i;
-    while(!feof(input))
-    {
-        fscanf (input, "%d", &i);
-        count++;
-    }
-    int numbers[count];
-    i = 0;
-    rewind(input);
-    while(!feof(input))
-    {
-        fscanf(input, "%d", &numbers[i]);
-        i++;
-    }
+    assert(argc >= 2); // you need at least an n and m value, even if they are 0
+
+}
+
+int* rref(int n, int m, int* matrix[]) {
     
-    fclose(input);
-    int count_of_less_than = 0;
-    for (int i = 0; i < count; ++i)
-    {
-        if (numbers[i+1] > numbers[i]) {
-            count_of_less_than++;
-        }
-    }
-    printf("%d", count_of_less_than);
-    // Part 1 answer: 1602
-    // Part 2
-    int sliding_window[count];
-    for (int i = 0; i < count - 2; ++i) {
-        sliding_window[i] = numbers[i+2] + numbers[i+1] + numbers[i];
-    }
-    printf("%d", sliding_window[2000]);
-    count_of_less_than = 0;
-    for (int i = 0; i < count - 2; ++i)
-    {
-        if (sliding_window[i+1] > sliding_window[i]) {
-            count_of_less_than++;
-        }
-    }
-    printf("%d", count_of_less_than);
-    // Part 2 Answer: 1633
 }
